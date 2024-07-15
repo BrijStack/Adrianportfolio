316c8fee6f501cd5edbf72728b0190cbff4938ec
BrijStack <brijesh17091999@gmail.com>
2024-07-15 16:05:19 +0530

    second commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -32,7 +32,11 @@ const Footer = () => {
               key={profile.id}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-black-200 rounded-lg border border-black-300"
             >
-              <img src={profile.img} alt={profile.id} width={20} height={20} />
+              <img
+                src={profile.img}
+                alt={profile.name}
+                className="w-[20px] h-[20px]"
+              />
             </div>
           ))}
         </div>

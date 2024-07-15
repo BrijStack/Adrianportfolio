9f3e4c6bfdea0e8e21a2a50b564c7f307657c450
BrijStack <brijesh17091999@gmail.com>
2024-07-15 16:24:23 +0530

    image changes commit 2

--- data/index.ts
+++ data/index.ts
@@ -75,7 +75,13 @@ export const projects = [
     title: "3D Solar System Planets to Explore",
     des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
     img: "./p1.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    iconLists: [
+      "./re.svg",
+      "./tail.svg",
+      "./ts.svg",
+      "./three.svg",
+      "./fm.svg",
+    ],
     link: "https://github.com/adrianhajdin?tab=repositories",
   },
   {
@@ -83,7 +89,13 @@ export const projects = [
     title: "Yoom - Video Conferencing App",
     des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
     img: "./p2.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    iconLists: [
+      "./next.svg",
+      "./tail.svg",
+      "./ts.svg",
+      "./stream.svg",
+      "./c.svg",
+    ],
     link: "https://github.com/adrianhajdin/zoom-clone",
   },
   {
@@ -91,7 +103,7 @@ export const projects = [
     title: "AI Image SaaS - Canva Application",
     des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
     img: "./p3.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    iconLists: ["./re.svg", "./tail.svg", "./ts.svg", "./three.svg", "./c.svg"],
     link: "https://github.com/adrianhajdin/ai_saas_app",
   },
   {
@@ -99,7 +111,13 @@ export const projects = [
     title: "Animated Apple Iphone 3D Website",
     des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
     img: "./p4.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    iconLists: [
+      "./next.svg",
+      "./tail.svg",
+      "./ts.svg",
+      "./three.svg",
+      "./gsap.svg",
+    ],
     link: "https://github.com/adrianhajdin/iphone",
   },
 ];
@@ -142,31 +160,31 @@ export const companies = [
     id: 1,
     name: "cloudinary",
     img: "./cloud.svg",
-    nameImg: "/cloudName.svg",
+    nameImg: "./cloudName.svg",
   },
   {
     id: 2,
     name: "appwrite",
     img: "./app.svg",
-    nameImg: "/appName.svg",
+    nameImg: "./appName.svg",
   },
   {
     id: 3,
     name: "HOSTINGER",
     img: "./host.svg",
-    nameImg: "/hostName.svg",
+    nameImg: "./hostName.svg",
   },
   {
     id: 4,
     name: "stream",
     img: "./s.svg",
-    nameImg: "/streamName.svg",
+    nameImg: "./streamName.svg",
   },
   {
     id: 5,
     name: "docker.",
     img: "./dock.svg",
-    nameImg: "/dockerName.svg",
+    nameImg: "./dockerName.svg",
   },
 ];
 
